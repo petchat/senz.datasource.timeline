acf418172e5be7938092c24c9f395052e93811c0
meowoodie <woodielove@163.com>
2015-04-26 12:53:30 +0800

    debug finish

--- cloud/dao.js
+++ cloud/dao.js
@@ -1,20 +1,15 @@
 /**
  * Created by woodie on 4/23/15.
  */
-//var untreatedData = {
-//    'UserLocation': [],
-//    'UserMotion': [],
-//    'UserSound': []
-//};
 
-var _getUntreatedData = function (UserRawdata) {
+var _getUntreatedData = function (UserRawdata){
     var promise = new AV.Promise();
     var user_rawdata = AV.Object.extend(UserRawdata);
     var query = new AV.Query(user_rawdata);
     query.equalTo('processStatus', 'untreated');
     query.find().then(
         function (result) {
-            console.log('\nFrom ' + UserRawdata);
+            console.log('From ' + UserRawdata);
             console.log('Successfully retrieved ' + result.length + ' untreated raw data.');
             var untreatedData = [];
             for (var i = 0; i < result.length; i++) {
@@ -23,32 +18,74 @@ var _getUntreatedData = function (UserRawdata) {
                     'rawdataId': result[i].get('userRawdataId'),
                     'timestamp': result[i].get('timestamp')
                 };
-                console.log('* id: ' + result[i].id + '\n* timestamp: ' + result[i].get('timestamp'));
+                console.log('* id: ' + result[i].id + '\n  timestamp: ' + result[i].get('timestamp'));
                 untreatedData.push(data);
             }
             promise.resolve(untreatedData);
         },
         function (error_info) {
-            console.log('\nError occurs! ' + error_info.code + ' ' + error_info.message);
+            console.log('  Error occurs! ' + error_info.code + ' ' + error_info.message);
             promise.reject(error_info);
         }
     );
     return promise;
 };
 
-exports.getUntreatedData = function (callback) {
+var _labelRawdataSenzed = function (UserRawdata, rawdata_id){
+    console.log('* id: ' + rawdata_id + ' in Class ' + UserRawdata);
+    var promise = new AV.Promise();
+    var user_rawdata = AV.Object.extend(UserRawdata);
+    var query = new AV.Query(user_rawdata);
+    query.get(rawdata_id, {
+        success: function(userRawdata){
+            var date = new Date();
+            userRawdata.set('processStatus', 'untreated');
+            userRawdata.set('senzedAt', date);
+            userRawdata.save().then(
+                function(obj){
+                    console.log('  Update status successfully!');
+                    promise.resolve(obj);
+                },
+                function(error_info){
+                    console.log('  Failed to update status, with error code: ' + error_info.message);
+                    promise.reject(error_info);
+                }
+            );
+        },
+        error: function(object, error_info){
+            console.log('Error occurs! ' + error_info.code + ' ' + error_info.message);
+        }
+    });
+    return promise;
+};
+
+exports.getUntreatedRawdata = function (){
     console.log('\nRetrieving untreated data...');
-    AV.Promise.when(
+    return AV.Promise.when(
         _getUntreatedData('UserLocation'),
         _getUntreatedData('UserMotion'),
         _getUntreatedData('UserSound')
-    ).then(function (user_location, user_motion, user_sound){
-            callback(user_location, user_motion, user_sound)
-    });
+    );
 };
 
-exports.addSenz = function (location_obj_id, motion_obj_id, sound_obj_id, timestamp, callback) {
+exports.labelRawdataSenzed = function (location_id_list, motion_id_list, sound_id_list){
+    console.log('\nMake the corresponding rawdata treated...');
+    var promises = [];
+    for (var id in location_id_list){
+        promises.push(_labelRawdataSenzed('UserLocation', location_id_list[id]));
+    }
+    for (var id in motion_id_list){
+        promises.push(_labelRawdataSenzed('UserMotion', motion_id_list[id]));
+    }
+    for (var id in sound_id_list){
+        promises.push(_labelRawdataSenzed('UserSound',sound_id_list[id]));
+    }
+    return AV.Promise.when(promises);
+};
+
+exports.addSenz = function (location_obj_id, motion_obj_id, sound_obj_id, timestamp){
     console.log('\nAdding new generated senz to database...');
+    var promise = new AV.Promise();
     var Senz = AV.Object.extend('UserSenz');
     var senz = new Senz();
     var motion_pointer = AV.Object.createWithoutData('UserMotion', motion_obj_id);
@@ -58,17 +95,19 @@ exports.addSenz = function (location_obj_id, motion_obj_id, sound_obj_id, timest
     senz.set('userLocation', location_pointer);
     senz.set('userSound', sound_pointer);
     senz.set('timestamp', timestamp);
-    senz.save(null, {
-        success: function (senz) {
-            console.log('\nNew Senz object created with objectId: ' + senz.id);
-            callback(senz.id);
+    senz.save().then(
+        function (senz){
+            console.log('New Senz object created with objectId: ' + senz.id);
+            promise.resolve(senz.id);
         },
-        error: function (senz, error) {
-            console.log('\nFailed to create new Senz object, with error code: ' + error.message);
+        function (senz, error_info) {
+            console.log('Failed to create new Senz object, with error code: ' + error.message);
+            promise.reject(error_info);
         }
-    });
-
+    );
+    return promise;
 };
+
 
 
 
--- cloud/main.js
+++ cloud/main.js
@@ -1,18 +1,54 @@
 // Use AV.Cloud.define to define as many cloud functions as you want.
 // For example:
-var dao = require('cloud/dao.js');
+var dao  = require('cloud/dao.js');
+var algo = require('cloud/algo.js');
 
-AV.Cloud.define("hello", function(request, response) {
-    // Get the all untreated raw data.
-    dao.getUntreatedData(function(user_location, user_motion, user_sound){
-        var date = new Date();
-        dao.addSenz(user_location[0]['objectId'], user_motion[0]['objectId'], user_sound[0]['objectId'], date, function(senz_id){
-//            console.log(senz_id);
-        });
-    });
-
+AV.Cloud.define("hello", function (request, response){
+    // Get untreated data from LeanCloud.
+    dao.getUntreatedRawdata().then(
+        // Request the senz collector with untreated data
+        // to get the list of senz tuples.
+        function (user_location_list, user_motion_list, user_sound_list){
+            request_data = {
+                'location': user_location_list,
+                'motion': user_motion_list,
+                'sound': user_sound_list
+            };
+            return algo.senzCollector(request_data);
+        }
+    ).then(
+        // Save the list of senz tuples to LeanCloud.
+        function (senz_list){
+//            var date = new Date();
+//            return dao.addSenz(location_id, motion_id, sound_id, date);
+        }
+    ).then(
+        // When everything is done.
+        function (){
+            var date = new Date();
+            console.log('\nEvery work has been done at ' + date);
+        }
+    );
     response.success("Hello world!");
 });
 
+//dao.labelRawdataSenzed(
+//    ['5538b907e4b0df9cbc4ca89c'],
+//    ['5538bf73e4b0df9cbc4dfb42'],
+//    ['5538bf87e4b0df9cbc4dfde4']
+//).then(
+//    function (){
+//        console.log('OK!');
+//    }
+//);
+data = {
+    "filter": 1,
+    "timelines": {
+        "location": [{"timestamp": 2}, {"timestamp": 4}, {"timestamp": 6}, {"timestamp": 9}],
+        "motion": [{"timestamp": 3}, {"timestamp": 4}, {"timestamp": 7}, {"timestamp": 9}],
+        "sound": [{"timestamp": 1}, {"timestamp": 3}, {"timestamp": 6}]
+    },
+    "primary_key": "location"
+};
 
-
+algo.senzCollector(data);
