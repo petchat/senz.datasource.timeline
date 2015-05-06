e731123addd498067aaf94b5058340dbc58955cb
meowoodie <woodielove@163.com>
2015-05-06 16:27:21 +0800

    modify the behavior collector url

--- cloud/config.js
+++ cloud/config.js
@@ -14,8 +14,8 @@ exports.url = {
     'test_url': 'http://httpbin.org/post',
     //'local_debug_url': 'http://127.0.0.1:9009/collector/',
     'senz_collector_url': 'http://120.27.30.239:9123/',
-    //'behavior_collector_url': 'http://120.27.30.239:9122/behavior_collector/',
-    'behavior_collector_url': 'http://127.0.0.1:9010/behavior_collector/',
+    'behavior_collector_url': 'http://120.27.30.239:9122/behavior_collector/',
+    //'behavior_collector_url': 'http://127.0.0.1:9010/behavior_collector/',
     'time_type_url': 'http://127.0.0.1:9011/time_type/',
     'analyser_url': ''
 };
