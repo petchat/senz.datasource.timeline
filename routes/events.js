c28c90e707476ee05f8f66cf836a36d82d844be2
meowoodie <woodielove@163.com>
2015-06-25 21:03:06 +0800

    add interface for user events

--- routes/events.js
+++ routes/events.js
@@ -1,10 +1,27 @@
 var router = require("express").Router();
 var AV     = require("leanengine");
+var dao    = require("../lib/dao.js");
 
-router.get("/:user_id", function (req, res, next){
+router.get("/:user_id", function (req, res){
     var user_id = req.params.user_id;
+    dao.getLatestEvents(user_id).then(
+        function (result){
+            var result_obj = {
+                code: 0,
+                result: result
+            };
+            res.json(result_obj);
+        },
+        function (error){
+            console.log(error);
+            var result_obj = {
+                code: 1,
+                message: error
+            };
+            res.json(result_obj);
+        }
+    );
 
-    res.json({ userId: user_id });
 });
 
 module.exports = router;
