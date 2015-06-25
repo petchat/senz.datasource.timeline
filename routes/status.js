58b288801ec11eb1210600fb9f8c492c09ab0a37
meowoodie <woodielove@163.com>
2015-06-25 19:48:36 +0800

    add new router for status

--- routes/status.js
+++ routes/status.js
@@ -1,10 +1,26 @@
 var router = require("express").Router();
 var AV     = require("leanengine");
+var dao    = require("../lib/dao.js");
 
-router.get("/:user_id", function (req, res, next){
+router.get("/:user_id", function (req, res){
     var user_id = req.params.user_id;
-
-    res.json({ userId: user_id });
+    dao.getLatestIntegratedSenz(user_id).then(
+        function (result){
+            var result_obj = {
+                code: 0,
+                result: result
+            };
+            res.json(result_obj);
+        },
+        function (error){
+            console.log(error);
+            var result_obj = {
+                code: 1,
+                message: error
+            };
+            res.json(result_obj);
+        }
+    );
 });
 
 module.exports = router;
