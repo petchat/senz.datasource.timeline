a92f674e3ab861bb9556ae20d2758430b68d5abc
mageia <yzg96@gmail.com>
2015-11-05 14:41:51 +0800

    comment route

--- routes/portal.js
+++ routes/portal.js
@@ -5,136 +5,136 @@ var _      = require("underscore");
 var serialize_task = require("../lib/serialize_task.js");
 
 
-router.get('/', function(req, res, next) {
-    dao.getAllUsers().then(
-        function (user_id_list){
-            var user_status_list = [];
-            var jobs_list = user_id_list;
-            var work      = serialize_task.SerializeTask();
-            var worker    = function (job, resolve, reject){
-                dao.getLatestIntegratedSenz(job).then(
-                    function (user_status){
-                        var description = "User "  + job;
-                        // Add motion description.
-                        if (_.has(user_status, "userMotion") && _.has(user_status["userMotion"], "motionProb")){
-                            var motion      = _.keys(user_status["userMotion"]["motionProb"]);
-                            var m_probs     = _.values(user_status["userMotion"]["motionProb"]);
-                            var m_max_prob  = 0;
-                            var m_max_index = 0;
-                            for (var i = 0; i < motion.length; i ++){
-                                if (m_probs[i] > m_max_prob){
-                                    m_max_prob  = m_probs[i];
-                                    m_max_index = i;
-                                }
-                            }
-                            console.log(user_status["userMotion"]["motionProb"]);
-                            description += (" IS " + motion[m_max_index] + "(" + m_probs[m_max_index] + ")");
-                        }
-                        // Add location description.
-                        if (_.has(user_status, "userLocation") && _.has(user_status["userLocation"], "poiProbLv1")){
-                            var location    = _.keys(user_status["userLocation"]["poiProbLv1"]);
-                            var l_probs     = _.values(user_status["userLocation"]["poiProbLv1"]);
-                            var l_max_prob  = 0;
-                            var l_max_index = 0;
-                            for (var i = 0; i < location.length; i ++){
-                                if (l_probs[i] > l_max_prob){
-                                    l_max_prob  = l_probs[i];
-                                    l_max_index = i;
-                                }
-                            }
-                            console.log(user_status["userLocation"]["poiProbLv1"]);
-                            description += (" AT " + location[l_max_index] + "(" + l_probs[l_max_index] + ")");
-                        }
-                        // Add sound description.
-                        if (_.has(user_status, "userSound") && _.has(user_status["userSound"], "soundProb")){
-                            var sound       = _.keys(user_status["userSound"]["soundProb"]);
-                            var s_probs     = _.values(user_status["userSound"]["soundProb"]);
-                            var s_max_prob  = 0;
-                            var s_max_index = 0;
-                            for (var i = 0; i < sound.length; i ++){
-                                if (s_probs[i] > s_max_prob){
-                                    s_max_prob  = s_probs[i];
-                                    s_max_index = i;
-                                }
-                            }
-                            console.log(user_status["userSound"]["soundProb"]);
-                            description += (" IN " + sound[s_max_index] + "(" + s_probs[s_max_index] + ")");
-                        }
-                        // Add Timestamp.
-                        description += (" AT TIME OF " + user_status["updatedAt"]);
-
-                        console.log(description);
-                        user_status_list.push(description);
-                        resolve();
-                    },
-                    function (error){
-                        var description = "User "  + job + " " + error;
-                        user_status_list.push(description);
-                        reject(error);
-                    }
-                );
-            };
-            jobs_list.forEach(function (job){
-                work.addTask(job);
-            });
-            work.setWorker(worker);
-            return work.begin().then(
-                function (){
-                    return AV.Promise.as(user_id_list, user_status_list);
-                }
-            );
-        }
-    ).then(
-        function (user_id_list, user_status_list){
-            console.log(user_id_list.length + user_status_list.length);
-            console.log(user_id_list);
-            var user_events_list = [];
-            var jobs_list = user_id_list;
-            var work      = serialize_task.SerializeTask();
-            var worker    = function (job, resolve, reject){
-                dao.getEvents(job, 1).then(
-                    function (user_events){
-                        var description = "User " + job + "'s EVENT is ";
-                        if (!_.has(user_events[0], "event")){
-                            description += "VACANT";
-                        }
-                        else {
-                            var events = _.keys(user_events[0]["event"]);
-                            var e_probs = _.values(user_events[0]["event"]);
-                            var e_max_prob = 0;
-                            var e_max_index = 0;
-                            for (var i = 0; i < events.length; i++) {
-                                if (e_probs[i] > e_max_prob) {
-                                    e_max_prob = e_probs[i];
-                                    e_max_index = i;
-                                }
-                            }
-                            description += (events[e_max_index] + "(" + e_probs[e_max_index] + ")" + "AT TIME OF " + user_events[0]["updatedAt"]);
-                        }
-                        user_events_list.push(description);
-                        resolve();
-                    },
-                    function (error){
-                        var description = "User "  + job + " " + error;
-                        user_events_list.push(description);
-                        reject(error);
-                    }
-                );
-            };
-            jobs_list.forEach(function (job){
-                work.addTask(job);
-            });
-            work.setWorker(worker);
-            return work.begin().then(
-                function (){
-                    res.render("portal", {
-                        user_status: user_status_list,
-                        user_events: user_events_list
-                    });
-                }
-            );
-        }
-    );
-});
+//router.get('/', function(req, res, next) {
+//    dao.getAllUsers().then(
+//        function (user_id_list){
+//            var user_status_list = [];
+//            var jobs_list = user_id_list;
+//            var work      = serialize_task.SerializeTask();
+//            var worker    = function (job, resolve, reject){
+//                dao.getLatestIntegratedSenz(job).then(
+//                    function (user_status){
+//                        var description = "User "  + job;
+//                        // Add motion description.
+//                        if (_.has(user_status, "userMotion") && _.has(user_status["userMotion"], "motionProb")){
+//                            var motion      = _.keys(user_status["userMotion"]["motionProb"]);
+//                            var m_probs     = _.values(user_status["userMotion"]["motionProb"]);
+//                            var m_max_prob  = 0;
+//                            var m_max_index = 0;
+//                            for (var i = 0; i < motion.length; i ++){
+//                                if (m_probs[i] > m_max_prob){
+//                                    m_max_prob  = m_probs[i];
+//                                    m_max_index = i;
+//                                }
+//                            }
+//                            console.log(user_status["userMotion"]["motionProb"]);
+//                            description += (" IS " + motion[m_max_index] + "(" + m_probs[m_max_index] + ")");
+//                        }
+//                        // Add location description.
+//                        if (_.has(user_status, "userLocation") && _.has(user_status["userLocation"], "poiProbLv1")){
+//                            var location    = _.keys(user_status["userLocation"]["poiProbLv1"]);
+//                            var l_probs     = _.values(user_status["userLocation"]["poiProbLv1"]);
+//                            var l_max_prob  = 0;
+//                            var l_max_index = 0;
+//                            for (var i = 0; i < location.length; i ++){
+//                                if (l_probs[i] > l_max_prob){
+//                                    l_max_prob  = l_probs[i];
+//                                    l_max_index = i;
+//                                }
+//                            }
+//                            console.log(user_status["userLocation"]["poiProbLv1"]);
+//                            description += (" AT " + location[l_max_index] + "(" + l_probs[l_max_index] + ")");
+//                        }
+//                        // Add sound description.
+//                        if (_.has(user_status, "userSound") && _.has(user_status["userSound"], "soundProb")){
+//                            var sound       = _.keys(user_status["userSound"]["soundProb"]);
+//                            var s_probs     = _.values(user_status["userSound"]["soundProb"]);
+//                            var s_max_prob  = 0;
+//                            var s_max_index = 0;
+//                            for (var i = 0; i < sound.length; i ++){
+//                                if (s_probs[i] > s_max_prob){
+//                                    s_max_prob  = s_probs[i];
+//                                    s_max_index = i;
+//                                }
+//                            }
+//                            console.log(user_status["userSound"]["soundProb"]);
+//                            description += (" IN " + sound[s_max_index] + "(" + s_probs[s_max_index] + ")");
+//                        }
+//                        // Add Timestamp.
+//                        description += (" AT TIME OF " + user_status["updatedAt"]);
+//
+//                        console.log(description);
+//                        user_status_list.push(description);
+//                        resolve();
+//                    },
+//                    function (error){
+//                        var description = "User "  + job + " " + error;
+//                        user_status_list.push(description);
+//                        reject(error);
+//                    }
+//                );
+//            };
+//            jobs_list.forEach(function (job){
+//                work.addTask(job);
+//            });
+//            work.setWorker(worker);
+//            return work.begin().then(
+//                function (){
+//                    return AV.Promise.as(user_id_list, user_status_list);
+//                }
+//            );
+//        }
+//    ).then(
+//        function (user_id_list, user_status_list){
+//            console.log(user_id_list.length + user_status_list.length);
+//            console.log(user_id_list);
+//            var user_events_list = [];
+//            var jobs_list = user_id_list;
+//            var work      = serialize_task.SerializeTask();
+//            var worker    = function (job, resolve, reject){
+//                dao.getEvents(job, 1).then(
+//                    function (user_events){
+//                        var description = "User " + job + "'s EVENT is ";
+//                        if (!_.has(user_events[0], "event")){
+//                            description += "VACANT";
+//                        }
+//                        else {
+//                            var events = _.keys(user_events[0]["event"]);
+//                            var e_probs = _.values(user_events[0]["event"]);
+//                            var e_max_prob = 0;
+//                            var e_max_index = 0;
+//                            for (var i = 0; i < events.length; i++) {
+//                                if (e_probs[i] > e_max_prob) {
+//                                    e_max_prob = e_probs[i];
+//                                    e_max_index = i;
+//                                }
+//                            }
+//                            description += (events[e_max_index] + "(" + e_probs[e_max_index] + ")" + "AT TIME OF " + user_events[0]["updatedAt"]);
+//                        }
+//                        user_events_list.push(description);
+//                        resolve();
+//                    },
+//                    function (error){
+//                        var description = "User "  + job + " " + error;
+//                        user_events_list.push(description);
+//                        reject(error);
+//                    }
+//                );
+//            };
+//            jobs_list.forEach(function (job){
+//                work.addTask(job);
+//            });
+//            work.setWorker(worker);
+//            return work.begin().then(
+//                function (){
+//                    res.render("portal", {
+//                        user_status: user_status_list,
+//                        user_events: user_events_list
+//                    });
+//                }
+//            );
+//        }
+//    );
+//});
 
 module.exports = router;
--- routes/service.js
+++ routes/service.js
@@ -3,104 +3,104 @@ var AV     = require("leanengine");
 var dao    = require("../lib/dao.js");
 var sp     = require("../lib/senz_process.js");
 
-router.get("/:userId/context", function (req, res){
-    var user_id = req.params.userId,
-        start_time = parseInt(req.query.startTime),
-        end_time   = parseInt(req.query.endTime),
-        scale      = req.query.scale;
+//router.get("/:userId/context", function (req, res){
+//    var user_id = req.params.userId,
+//        start_time = parseInt(req.query.startTime),
+//        end_time   = parseInt(req.query.endTime),
+//        scale      = req.query.scale;
+//
+//    if (start_time != undefined && end_time != undefined && scale != undefined){
+//        if (end_time - start_time > 1000*3600*24){
+//            res.json({
+//                code: 2,
+//                message: "The range of time scale should not be longer than one day."
+//            });
+//        }
+//        else {
+//            sp.behaviorGenerator(user_id, start_time, end_time, scale, false).then(
+//                function (historical_senzes) {
+//                    var result = [];
+//                    historical_senzes.forEach(function (senz) {
+//                        if (senz["senzId"].length > 0) {
+//                            var _senz = {
+//                                "userMotion": {
+//                                    "motionProb": senz["motionProb"]
+//                                },
+//                                "userSound": {
+//                                    "soundProb": senz["soundProb"]
+//                                },
+//                                "userLocation": {
+//                                    "poiProbLv1": senz["poiProbLv1"],
+//                                    "poiProbLv2": senz["poiProbLv2"]
+//                                },
+//                                "count": senz["senzId"].length,
+//                                "updatedAt": new Date(senz["timestamp"])
+//                            };
+//                            result.push(_senz);
+//                        }
+//                    });
+//                    res.json({
+//                        code: 0,
+//                        historicalSenzes: result
+//                    });
+//                },
+//                function (error) {
+//                    res.json({
+//                        code: 1,
+//                        message: error
+//                    });
+//                }
+//            );
+//        }
+//    }
+//    else{
+//        dao.getLatestIntegratedSenz(user_id).then(
+//            function (result){
+//                var result_obj = {
+//                    code: 0,
+//                    result: result
+//                };
+//                res.json(result_obj);
+//            },
+//            function (error){
+//                console.log(error);
+//                var result_obj = {
+//                    code: 1,
+//                    message: error
+//                };
+//                res.json(result_obj);
+//            }
+//        );
+//    }
+//});
 
-    if (start_time != undefined && end_time != undefined && scale != undefined){
-        if (end_time - start_time > 1000*3600*24){
-            res.json({
-                code: 2,
-                message: "The range of time scale should not be longer than one day."
-            });
-        }
-        else {
-            sp.behaviorGenerator(user_id, start_time, end_time, scale, false).then(
-                function (historical_senzes) {
-                    var result = [];
-                    historical_senzes.forEach(function (senz) {
-                        if (senz["senzId"].length > 0) {
-                            var _senz = {
-                                "userMotion": {
-                                    "motionProb": senz["motionProb"]
-                                },
-                                "userSound": {
-                                    "soundProb": senz["soundProb"]
-                                },
-                                "userLocation": {
-                                    "poiProbLv1": senz["poiProbLv1"],
-                                    "poiProbLv2": senz["poiProbLv2"]
-                                },
-                                "count": senz["senzId"].length,
-                                "updatedAt": new Date(senz["timestamp"])
-                            };
-                            result.push(_senz);
-                        }
-                    });
-                    res.json({
-                        code: 0,
-                        historicalSenzes: result
-                    });
-                },
-                function (error) {
-                    res.json({
-                        code: 1,
-                        message: error
-                    });
-                }
-            );
-        }
-    }
-    else{
-        dao.getLatestIntegratedSenz(user_id).then(
-            function (result){
-                var result_obj = {
-                    code: 0,
-                    result: result
-                };
-                res.json(result_obj);
-            },
-            function (error){
-                console.log(error);
-                var result_obj = {
-                    code: 1,
-                    message: error
-                };
-                res.json(result_obj);
-            }
-        );
-    }
-});
-
-router.get("/:userId/events", function (req, res){
-    var user_id    = req.params.userId,
-        limit      = req.query.limit,
-        start_time = req.query.startTime,
-        end_time   = req.query.endTime;
-
-    if (limit == undefined && start_time == undefined && end_time == undefined){
-        limit = 1;
-    }
-    dao.getEvents(user_id, limit, start_time, end_time).then(
-        function (result){
-            var result_obj = {
-                code: 0,
-                result: result
-            };
-            res.json(result_obj);
-        },
-        function (error){
-            console.log(error);
-            var result_obj = {
-                code: 1,
-                message: error
-            };
-            res.json(result_obj);
-        }
-    );
-});
+//router.get("/:userId/events", function (req, res){
+//    var user_id    = req.params.userId,
+//        limit      = req.query.limit,
+//        start_time = req.query.startTime,
+//        end_time   = req.query.endTime;
+//
+//    if (limit == undefined && start_time == undefined && end_time == undefined){
+//        limit = 1;
+//    }
+//    dao.getEvents(user_id, limit, start_time, end_time).then(
+//        function (result){
+//            var result_obj = {
+//                code: 0,
+//                result: result
+//            };
+//            res.json(result_obj);
+//        },
+//        function (error){
+//            console.log(error);
+//            var result_obj = {
+//                code: 1,
+//                message: error
+//            };
+//            res.json(result_obj);
+//        }
+//    );
+//});
 
 
 module.exports = router;
